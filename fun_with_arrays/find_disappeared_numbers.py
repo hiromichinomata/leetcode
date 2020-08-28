cdd439806bbb72d4c0c77b7c1f8b1b65fc45fdd5
Hiromichi NOMATA <git@hiromichinomata.com>
2020-08-28 09:23:16 +0900

    [fix] find disappeared numbers

--- fun_with_arrays/find_disappeared_numbers.py
+++ fun_with_arrays/find_disappeared_numbers.py
@@ -1,7 +1,13 @@
 class Solution:
     def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
-        res = []
+        memo = {}
         for i in range(1, len(nums)+1):
-            if i not in nums:
-                res.append(i)
-        return res
+            memo[i] = -1
+        for i in nums:
+            memo[i] += 1
+
+        res = []
+        for k, v in memo.items():
+            if v == -1:
+                res.append(k)
+        return sorted(res)
